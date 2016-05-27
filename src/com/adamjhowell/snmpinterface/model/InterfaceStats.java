524f1881ee6b07511185e6a5c5d803ed2e9e8546
Adam Howell <adam.j.howell@gmail.com>
2016-05-27 12:06:42 -0600

    29 - Added Maven, fixed comments.
    I want to move my code to the Maven folder, or move Maven to the existing source location.
     I added a number of comments (mostly to the model classes) and fixed a few others.

--- src/com/adamjhowell/snmpinterface/model/InterfaceStats.java
+++ src/com/adamjhowell/snmpinterface/model/InterfaceStats.java
@@ -2,8 +2,10 @@ package com.adamjhowell.snmpinterface.model;
 
 
 /**
- * Created by Adam Howell
- * on 2016-05-25.
+ * Created by Adam Howell on 2016-05-25.
+ * This class is meant to hold data that is fed to a JavaFX TableView object (statisticTableView).
+ * The "description" class member will go into column 1, named "Description".
+ * The "value" class member will go into column 2, named "Value".
  */
 public class InterfaceStats
 {
@@ -11,6 +13,12 @@ public class InterfaceStats
 	private String value;
 
 
+	/**
+	 * InterfaceStats parameterized constructor.
+	 *
+	 * @param description a description of the value.
+	 * @param value       the value/stat to display.
+	 */
 	public InterfaceStats( String description, String value )
 	{
 		this.description = description;
@@ -30,6 +38,10 @@ public class InterfaceStats
 	}
 
 
+	/**
+	 * I wanted a cleaner toString().
+	 * @return a string representing the object.
+	 */
 	@Override
 	public String toString()
 	{
